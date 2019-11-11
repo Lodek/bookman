e997eefa5461971d188d33d554b9111d05fe83fb
Lodek <23224552+Lodek@users.noreply.github.com>
2019-11-11 15:07:13 -0300

    removed ramble

--- bookman/model.py
+++ bookman/model.py
@@ -1,33 +1,5 @@
 """
-ok, what do I want?
-a simple book organizer app
-insert book through file, isbn
-keeps all books in a book directory
-book alias
-open book by alias
-fetch book metadata through web api
-
-why do I want/need?
-Calibre is a pain.
-is it tho?
-a lil bit.
-i don't hate it but it has more than I need and it doesn't solve the thing that I want. i'd like my shit to be scriptable.
-so the question is, do you ACTUALLY need this? cause it's gonna cost time.
-overall I think it's a 'would be nice' situation because my books are pretty damn messy rn tbh.
-
-FINE.
-Just do it then.
-
-How are you doing searchs tho?
-I'd need to make a nice cli for it.
-with options.
-or maybe scriptable.
-idk honestly.
-for now i could consider searching the json file
-eventually i'd have to add a proper cli to it.
-
-moreover, i feel like this would be a good template for my bookmarks and mellori
-i wrote bookpoint but i never used it, never even got around to organizing those darn favorites' """
+"""
 
 import configparser, json, re
 from pathlib import Path
