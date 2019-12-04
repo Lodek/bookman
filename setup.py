a38ce8682c9e0818b047ac857c366b5cc64829b0
Lodek <23224552+Lodek@users.noreply.github.com>
2019-12-04 15:19:08 -0300

    fixed setup.py entry_point

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setuptools.setup(
     version='0.0.1',
     author='Lodek',
     description='Book manager',
-    packages=setuptools.find_packages(),
-    entrypoints={'console_scripts' : ['bookman = bookman.bookman:main'],
-                 'gui_scripts' : ['bookman = bookman.bookman:main']})
+    packages=['bookman'],
+    entry_points={
+        'console_scripts' : ['bookman = bookman.bookman:main'],
+        }
+    )
 
