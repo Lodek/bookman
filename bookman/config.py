812dd1cb512a2d26f161eb5a1ae43d1643869a40
Bruno Gomes <gomes.bruno.ac@gmail.com>
2021-05-07 16:34:07 -0300

    chore: removed dead code

--- bookman/config.py
+++
@@ -1,8 +0,0 @@
-books_json = '~/.config/bookman/books.json' #bookman json file
-books_dir = '~/books' #directory with books
-api_key = '' #googlebooks api key
-api_key_file = ''
-
-#Dictionary item with customizable fields which will be added to the model
-extra_attrs = {
-}
--- bookman/properties.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-bookman_config_dir = Path('~/.config/bookman').expanduser().absolute()
-default_config_path = bookman_config_dir / 'config.py'
-env_var_prefix = 'BOOKMAN_'
-default_config_body = 'from bookman.config import *\n'
-config_env_var_name = 'BOOKMAN_CONFIG'
-
